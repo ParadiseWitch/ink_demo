585345c6f4befa01d73a1d8721f3f684482f3843
Maiiiiiid <zhp92@outlook.com>
2021-08-28 12:21:44 +0800

    添加cursor指针

--- src/ui/scroll-view.tsx
+++ src/ui/scroll-view.tsx
@@ -3,61 +3,83 @@ import { Box, Text, Newline, useInput } from "ink";
 
 import React from "react";
 
-const getNumberLen = (number) => {
+const getNumberLen = (number: number): number => {
   return String(number).length;
 };
 
+const getMidLine = (startLine: number, endLine: number): number => {
+  return Math.round((startLine + endLine) / 2);
+};
 export const ScrollView: FC<{ data: string[]; limit: number }> = ({
   data,
   limit,
 }) => {
+  const getEndLine = () => {
+    return startLine + limit;
+  }
   const [startLine, setStartLine] = useState<number>(0);
-  // const [cursor, setCursor] = useState<number>(0);
+  const [cursor, setCursor] = useState<number>(0);
   const [showData, setShowData] = useState<string[]>(
-    data.slice(startLine, startLine + limit)
+    data.slice(startLine, getEndLine())
   );
 
   useInput((input, key) => {
     if (key.upArrow) {
-      if (startLine <= 0) {
+      if (cursor <= 0) {
+        setCursor(0);
         setStartLine(0);
       } else {
-        setStartLine(startLine - 1);
+        if (
+          startLine > 0 &&
+          cursor <= getMidLine(startLine + 1, getEndLine())
+        ) {
+          setStartLine(startLine - 1);
+        }
+        setCursor(cursor - 1);
       }
-      setShowData(data.slice(startLine, startLine + limit));
+      setShowData(data.slice(startLine, getEndLine()));
     }
     if (key.downArrow) {
-      console.log(data.length);
-      if (startLine + limit >= data.length) {
+      if (cursor >= data.length) {
+        setCursor(data.length);
         setStartLine(data.length - limit);
       } else {
-        setStartLine(startLine + 1);
+        if (getEndLine() < data.length && cursor >= getMidLine(startLine + 1, getEndLine())) {
+          setStartLine(startLine + 1);
+        }
+        setCursor(cursor + 1);
       }
-      setShowData(data.slice(startLine, startLine + limit));
+      setShowData(data.slice(startLine, getEndLine()));
     }
   });
 
   return (
     <>
       <Box borderStyle="round" flexDirection="column">
-        {showData.map((item, index) => (
-          <Box key={index}>
-            <Text color="blue">
-              {new Array(
-                getNumberLen(data.length) - getNumberLen(index + startLine + 1)
-              ).fill("\u00A0")}
-              {index + startLine + 1}
-              {"|"}
-              {"\u000B\u00A0"}
-            </Text>
-            <Text>{item}</Text>
-          </Box>
-        ))}
+        {showData.map((item, index) => {
+          return (
+            <Box key={index}>
+              <Text>
+               {getEndLine()}
+              </Text>
+              <Text color="blue">
+                {cursor == index + startLine + 1 && ">"}
+                {cursor != index + startLine + 1 && "\u00A0"}
+                {"\u00A0"}
+                {new Array(
+                  getNumberLen(data.length) -
+                    getNumberLen(index + startLine + 1)
+                ).fill("\u00A0")}
+                {index + startLine + 1}
+                {"|"}
+                {"\u000B"}
+              </Text>
+              <Text>{item}</Text>
+            </Box>
+          );
+        })}
       </Box>
       <Newline />
-      <Text color="red">
-        {startLine + 1}-{startLine + limit}
-      </Text>
     </>
   );
 };
