c8bac8cb7045ca936751cc872afb44b1503d2dc1
Maiiiiiid <zhp92@outlook.com>
2021-08-28 18:55:49 +0800

    移动外部option.ts到内部

--- src/ui/option/option.ts
+++
@@ -1,9 +0,0 @@
-export default class option {
-  public label: string;
-  public value: string;
-  constructor(label: string, value?: string) {
-    if (!value) this.label = this.value = label;
-    this.label = label;
-    this.value = value;
-  }
-}
