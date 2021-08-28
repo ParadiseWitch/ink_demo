f25fadd60313570c242a5786ce21e3b494987205
Maiiiiiid <zhp92@outlook.com>
2021-08-28 18:54:43 +0800

    子进程命令执行函数调用

--- src/build/build.ts
+++ src/build/build.ts
@@ -1,10 +1,11 @@
 import { envs } from "../config"
 import { exec } from "../utils"
 import { drive, toolsDir } from "../config"
+import { subProcessOut } from "../subProcessOut"
 
 export class Build {
   isDev: boolean
-  cmd: any
+  cmd: string
   targetDir: string
   buildModules: string[]
   constructor(envName: string) {
@@ -27,18 +28,20 @@ export class Build {
   }
 
   // 运行打包命令
-  async runBuildCommand() {
+  async runBuildCommand(): Promise<string> {
+    let out: string[] = [];
     for (let i = 0; i < this.buildModules.length; i++) {
       const command = `${drive} && cd ${toolsDir}\\${this.buildModules[i]} && ${this.cmd}`
-      await exec(command)
+      out.push.apply(await subProcessOut(command))
     }
+    return out.join("\n");
   }
 
   // 复制到目标目录
-  async dup2targetDir() {
+  async dup2targetDir(): Promise<string> {
     for (let i = 0; i < this.buildModules.length; i++) {
       const command = `echo d | xcopy ${toolsDir}\\${this.buildModules[i]}\\dist\\${this.buildModules[i]} ${this.targetDir}\\${this.buildModules[i]} /s/y`
-      await exec(command)
+      return await subProcessOut(command)
     }
   }
 }
